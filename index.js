e843240a64cd0d50438fba1117260a34b6e7058d
lleejae <dlwogur5020@naver.com>
2021-03-28 11:59:06 +0900

    mongo 게시판 마무리

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ var upload = multer({ storage: storage });
 var postSchema = mongoose.Schema( {
   title: {type:String, required:true},
   writer: {type:String, required:true},
-  body: {type:String, required:true},
+  body: {type:String},
   date: {type:Date, default:Date.now},
   file: {type:String},
-  password: {type:Number}
+  password: {type:Number},
+  num: {type:Number}
 });
 
 var Post = mongoose.model('postit', postSchema);
@@ -53,30 +54,50 @@ app.get('/', function(req, res) {
   });
 });
 
-app.get('/posting', function(req, res){
+app.get('/posting', function(req, res){ // 게시글 작성 페이지: /posting으로 get
     res.render('pages/posting');
 });
 
-app.get('/post/:id', function(req, res){
+app.get('/post/:id', function(req, res){  // 게시글 입장은 /post/:id로 get
     Post.findOne({_id:req.params.id}, function(err, post) {
       if (err) res.json(err);
       res.render('pages/apost', {post:post});
     });
 });
 
-app.post('/post/:id', function(req, res) {
-  Post.deleteOne({_id:req.params.id}, function(err, post) {
-    if (err) res.json(err);
-    res.redirect('/');
-  });
+app.post('/post/:id', function(req, res) {  // 삭제 요청은 /post/:id 로 post
+    if (req.body.password == '4444') {  // 마스터키 4444
+      Post.deleteOne({_id:req.params.id}, function(err3, post) {
+        if (err3) res.json(err3);
+        res.redirect('/');
+      });
+    }
+    else {
+      Post.findOne({_id:req.params.id, password: req.body.password}, function(err, post) {
+        if (err) {
+          res.redirect('/');
+          return;
+        }
+        Post.deleteOne({_id:req.params.id, password: req.body.password}, function(err2, post) {
+          if (err2) res.json(err2);
+          res.redirect('/');
+        });
+      });
+    }
 });
 
-app.post('/posting', upload.single('image'), function(req, res) {
+app.post('/posting', upload.single('image'), function(req, res) { // 글 작성은 /posting으로 post
+    if (!(req.body.title && req.body.writer && req.body.body && req.body.password)){
+      res.redirect('/');
+      return;
+    }
+
     var newobject = {};
     for (var entry in req.body) {
       newobject[entry] = req.body[entry];
     }
-    newobject.file = req.file.path.substring(7);
+    if (req.file)
+      newobject.file = req.file.path.substring(6);
     Post.create(newobject, function(err, post) {
       if (err) res.json(err);
       res.redirect('/');
